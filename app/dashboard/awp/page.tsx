c6e16dde7adba4943bdaa805ee7c655ee760ba7c
Marceli Zaborowski <marcelikosmazaborowski@gmail.com>
2025-01-06 15:33:52 +0100

    Update page.tsx porting

--- app/dashboard/awp/page.tsx
+++ app/dashboard/awp/page.tsx
@@ -39,7 +39,7 @@ const Dashboard = () => {
   useEffect(() => {
     // Replace with actual fetch
     const fetchData = async () => {
-      const response = await fetch('http://98.67.165.93:8080/awpstate');
+      const response = await fetch('https://98.67.165.93:8443/awp_state');
       const data = await response.json();
       setData(data);
     };
